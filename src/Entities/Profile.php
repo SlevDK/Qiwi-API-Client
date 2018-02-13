28bf7a8cb91ac4d99d571818dbe967793db4ace5
SlevDK <slevin.dyachenko@gmail.com>
2018-02-14 01:23:37 +0200

    Add Profile entity (getProfile)

---
+++ src/Entities/Profile.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace QiwiApi\Entities;
+
+/**
+ * Class Profile
+ * @package QiwiApi\Entities
+ */
+class Profile extends RequestEntity
+{
+    /** @var string Request uri */
+    public $uri = "person-profile/v1/profile/current";
+
+    /**
+     * Prepare params for request
+     * 
+     * @param array $options Request data params
+     * @param string $token Qiwi wallet token
+     * 
+     * @return array
+     */
+    protected function prepareParams($options, $token)
+    {
+        $params["query"] = $options;
+
+        $params["headers"] = $this->prepareHeaders($token);
+        
+        return $params;
+    }
+
+    /**
+     * Prepare resource URI
+     * 
+     * @param string $baseURI Base uri
+     * @param string $wallet Qiwi wallet number
+     * 
+     * @return string
+     */
+    protected function prepareUri($baseURI, $wallet)
+    {
+        return $baseURI.$this->uri;
+    }
+}
